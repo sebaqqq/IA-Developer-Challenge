aa17ce6db8c2c9a93b6c3df4e3ba62ed3822fe53
sebaqqq <sgquintana2002@gmail.com>
2025-05-07 18:03:11 -0400

    new section title

--- src/components/UI/SectionTitle.tsx
+++ src/components/UI/SectionTitle.tsx
@@ -28,7 +28,7 @@ const SectionTitle: React.FC<SectionTitleProps> = ({
           }`}
         ></div>
       )}
-      <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-3">
+      <h2 className="text-3xl md:text-4xl font-bold text-gray-100 mb-3">
         {title}
       </h2>
       {subtitle && <p className="text-lg text-gray-400">{subtitle}</p>}
